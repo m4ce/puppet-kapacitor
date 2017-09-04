96e3bc7d1c26ac93e45a7df3b9d5d74e01e44695
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2017-09-04 17:13:35 +0100

    missing ensure in template type

--- types/template.pp
+++ types/template.pp
@@ -1,4 +1,5 @@
 type Kapacitor::Template = Struct[{
-  type => Enum["stream", "batch"],
-  script => String
+  "type" => Enum["stream", "batch"],
+  script => String,
+  Optional[ensure] => Enum["present", "absent"]
 }]
